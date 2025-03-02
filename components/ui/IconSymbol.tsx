79a67b2adb224f0c6ce7684bfcd6534b2cf4b10a
snarpeppo <snarpeppo98@gmail.com>
2025-03-02 13:46:40 +0100

    android setup

--- components/ui/IconSymbol.tsx
+++ components/ui/IconSymbol.tsx
@@ -1,6 +1,7 @@
 // This file is a fallback for using MaterialIcons on Android and web.
 
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+
 import { SymbolWeight } from 'expo-symbols';
 import React from 'react';
 import { OpaqueColorValue, StyleProp, ViewStyle } from 'react-native';
@@ -11,6 +12,8 @@ const MAPPING = {
   // See SF Symbols in the SF Symbols app on Mac.
   'house.fill': 'home',
   'paperplane.fill': 'send',
+  'hourglass':'hourglass-top',
+
   'chevron.left.forwardslash.chevron.right': 'code',
   'chevron.right': 'chevron-right',
 } as Partial<
